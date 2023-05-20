27f96f7e7e51ae42a3831104cd92bb7e5e8a88d2
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2023-05-20 11:24:53 +0200

    changeup

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     'nuxt-gsap-module',
-    '@nuxtjs/svg'
+    '@nuxtjs/svg',
+    'vue-slick'
   ],
 
   gsap: {
