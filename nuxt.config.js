10876c9b199a8fe4c02818761d9ee4c482ae56bb
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2023-05-25 22:34:10 +0200

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ROOTS',
+    title: 'ROOTS EXHIBITION',
     htmlAttrs: {
       lang: 'en'
     },
